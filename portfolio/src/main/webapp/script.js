3c8ed8c897af6ef3a052ef15cd2d37cbf7269b34
Temiloluwa <igetemiloluwa05@icloud.com>
2021-06-05 00:40:46 +0000

    :/

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,14 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomWord() {
+  const words =
+      ['Hello', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!', 'Heyyy', 'Welcome'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const word = words[Math.floor(Math.random() * words.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const wordContainer = document.getElementById('word-container');
+  wordContainer.innerText = word;
 }
